d79b10220b0a03f52ec3747907872ca9f85f3f27
Goncalo <goncalo_ramalho@hotmail.com>
2016-05-19 00:52:26 +0200

    show information as in console when pause/play/finish

--- plugin/js/counterPlugin.js
+++ plugin/js/counterPlugin.js
@@ -1,11 +1,34 @@
+
+
+function displayInformation(a, b) {
+	
+	var timePassed = a - b;//time elapsed between pause/resume
+	
+	if (!document.getElementById('info')) {
+		
+		//create container
+		console.log('no container for information');
+	}
+
+	//put information in container
+	console.log('time elapsed between pause/resume is: ' + timePassed);
+
+}
+
 function counterPlugin() {
   
 	//wait for videojs to be ready
 	videojs("my-video").ready(function(){
 
 		var thisVideo = this,
-			videoResumed = {},
-			videoPaused = {};
+			videoResumed = {
+				count: 0,
+				time: 0
+			},
+			videoPaused = {
+				count: 0,
+				time: 0
+			};
 
 		//action when video plays for the first time
 		thisVideo.one('play', function () {
@@ -17,13 +40,18 @@ function counterPlugin() {
 		thisVideo.on('play', function(e) {
 			
 			//if user is not seeking video
-			if(!thisVideo.seeking()) {
+			if(!thisVideo.seeking() && videoResumed) {
 				//send information through false http request
-				console.log('video resume');		
 				
-				console.log('is it seeking? ' + thisVideo.seeking());
 				//increment number of plays
-				//videoResumed.push(+=1, ) ;
+				videoResumed.count +=1;
+				
+				//save time of resuming
+				videoResumed.time = thisVideo.currentTime();
+
+				// console.log('Video resumed count: ' + videoResumed.count);
+				// console.log('Video resumed time: ' + videoResumed.time );
+
 			}
 		});
 
@@ -33,15 +61,19 @@ function counterPlugin() {
 			if(!thisVideo.seeking()) {
 
 				console.log('video paused');
-				console.log(thisVideo.currentTime());
 
-				console.log('is it seeking? ' + thisVideo.seeking());
 				//increment number of pauses
-				videoPaused +=1;
+				videoPaused.count +=1;
+				
+				//save time of pausing
+				videoPaused.time = thisVideo.currentTime();
 
 				//show information
-				console.log('Video resumed: ' + videoResumed);
-				console.log('Video paused: ' + videoPaused);
+				console.log('Video resume count: ' + videoResumed.count);
+				console.log('Video paused count: ' + videoPaused.count);
+				console.log('Video paused time: ' + videoPaused.time );
+
+				displayInformation(videoPaused.time, videoResumed.time);
 			}
 
 		});
