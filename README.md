0a5c44e76f52bd8cea6fc865e28be51d0aff93bf
Goncalo <goncalo_ramalho@hotmail.com>
2016-05-18 23:51:58 +0200

    prevent play/pause actions when seeking

--- README.md
+++ README.md
@@ -19,10 +19,6 @@ This is a video.js plugin that counts how many times the video is paused during 
 
 To install the plugin simply...
 
-## API Reference
-
-Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
-
 ## License
 
 A short snippet describing the license (MIT, Apache, etc.)
--- plugin/js/counterPlugin.js
+++ plugin/js/counterPlugin.js
@@ -1,33 +1,58 @@
-function counterPlugin(options) {
+function counterPlugin() {
   
-	var thisVideo = this;
+	//wait for videojs to be ready
+	videojs("my-video").ready(function(){
 
-	//action when video plays for the first time
-	thisVideo.one('play', function () {
-	  console.log('video starts for the first time');
-	  //send information through false http request (only on first play)
-	});
+		var thisVideo = this,
+			videoResumed = {},
+			videoPaused = {};
 
-	//action when video resumes
-	thisVideo.on('play', function(e) {
-		console.log('video resume');		
-		//increment number of plays
-	});
+		//action when video plays for the first time
+		thisVideo.one('play', function () {
+		  console.log('video starts for the first time');
+		  //send information through false http request
+		});
 
-	//action when video pauses
-	thisVideo.on('pause', function(e) {
-		console.log('video paused');
-		
-		//increment number of pauses
+		//action when video resumes
+		thisVideo.on('play', function(e) {
+			
+			//if user is not seeking video
+			if(!thisVideo.seeking()) {
+				//send information through false http request
+				console.log('video resume');		
+				
+				console.log('is it seeking? ' + thisVideo.seeking());
+				//increment number of plays
+				//videoResumed.push(+=1, ) ;
+			}
+		});
 
-		//show information
+		//action when video pauses
+		thisVideo.on('pause', function(e) {
+			
+			if(!thisVideo.seeking()) {
 
-	});
+				console.log('video paused');
+				console.log(thisVideo.currentTime());
 
-	//action when video finishes
-	thisVideo.on('ended', function(e) {
-		console.log('video finish');
-		//send information through false http request
+				console.log('is it seeking? ' + thisVideo.seeking());
+				//increment number of pauses
+				videoPaused +=1;
+
+				//show information
+				console.log('Video resumed: ' + videoResumed);
+				console.log('Video paused: ' + videoPaused);
+			}
+
+		});
+
+		//action when video finishes
+		thisVideo.on('ended', function(e) {
+			console.log('video finish');
+			//send information through false http request
+			//show information
+		});
+
 	});
 
 };
