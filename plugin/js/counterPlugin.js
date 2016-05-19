75557e8770c6fc81e5e91d34bcf04e4530044dcb
Goncalo <goncalo_ramalho@hotmail.com>
2016-05-19 14:58:15 +0200

    changed way to check for the first time video plays

--- plugin/js/counterPlugin.js
+++ plugin/js/counterPlugin.js
@@ -17,7 +17,7 @@ function displayInformation(a, b) {
 	if (!document.getElementById('info')) {
 		
 		//create container
-		console.log('no container for information');
+		//console.log('no container for information');
 	}
 
 	//put information in container
@@ -28,40 +28,56 @@ function displayInformation(a, b) {
 function counterPlugin() {
   
 	//wait for videojs to be ready
-	videojs("my-video").ready(function(){
+	videojs('my-video').ready(function(){
 
 		var thisVideo = this,
 			videoResumed = {
 				count: 0,
-				time: 0
+				time: 0,
+				firstPlay: false
 			},
 			videoPaused = {
 				count: 0,
 				time: 0
 			};
 
-		//action when video plays for the first time
-		thisVideo.one('play', function () {
-		  console.log('video starts for the first time');
-		  //send information through false http request
-		});
+		// //action when video plays for the first time
+		// thisVideo.one('play', function () {
+		//   console.log('video starts for the first time');
+		//   //send information through false http request
+
+		//   videoResumed.firstPlay = true;
+		// });
 
 		//action when video resumes
 		thisVideo.on('play', function(e) {
 			
 			//if user is not seeking video
-			if(!thisVideo.seeking() && videoResumed) {
-				//send information through false http request
+			if(!thisVideo.seeking()) {
 				
-				//increment number of plays
-				videoResumed.count +=1;
-				
-				//save time of resuming
-				videoResumed.time = thisVideo.currentTime();
+				//if it's the first time video starts
+				if (!videoResumed.firstPlay) {
+					
+					console.log('video starts for the first time');
+					//send information through false http request
 
-				// console.log('Video resumed count: ' + videoResumed.count);
-				// console.log('Video resumed time: ' + videoResumed.time );
+					videoResumed.firstPlay = true;					
+				}
+				else
+				{
+					//send information through false http request
+					
+					//increment number of plays
+					videoResumed.count +=1;
+					
+					//save time of resuming
+					videoResumed.time = thisVideo.currentTime();
 
+					// console.log('Video resumed count: ' + videoResumed.count);
+					console.log('Video resumed at: ' + videoResumed.time );
+					displayInformation(videoResumed.time, videoPaused.time);
+
+				}
 			}
 		});
 
@@ -79,9 +95,9 @@ function counterPlugin() {
 				videoPaused.time = thisVideo.currentTime();
 
 				//show information
-				console.log('Video resume count: ' + videoResumed.count);
-				console.log('Video paused count: ' + videoPaused.count);
-				console.log('Video paused time: ' + videoPaused.time );
+				// console.log('Video resume count: ' + videoResumed.count);
+				// console.log('Video paused count: ' + videoPaused.count);
+				console.log('Video paused at: ' + videoPaused.time );
 
 				displayInformation(videoPaused.time, videoResumed.time);
 			}
